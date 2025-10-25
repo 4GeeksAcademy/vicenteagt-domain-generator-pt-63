ae7753d8440247b4127820ec57941876b495d611
VicenteAGT <155429830+VicenteAGT@users.noreply.github.com>
2025-10-25 16:48:23 +0000

    [Developed app.js by vicenteagt] -Developed for cycles in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-import "bootstrap";
-import "./style.css";
+  let pronouns = ['the', 'our'];
+  let adjs = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
 
+  for(let indexPronouns = 0; indexPronouns < pronouns.length; indexPronouns++){
+    for(let indexAdjs = 0; indexAdjs < adjs.length; indexAdjs++){
+      for(let indexNouns = 0; indexNouns < nouns.length; indexNouns++){
+console.log(pronouns[indexPronouns]+adjs[indexAdjs]+nouns[indexNouns]+".com")
+  }
+  }
+  }
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
