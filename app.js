7afba9892f1aa81dfe690279bae2a182182ef2e6
VicenteAGT <155429830+VicenteAGT@users.noreply.github.com>
2025-10-27 23:13:01 +0000

    [Updates by vicenteagt] -Added a new app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+  let pronouns = ['the', 'our'];
+  let adjs = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+
+  for(let indexPronouns = 0; indexPronouns < pronouns.length; indexPronouns++){
+    for(let indexAdjs = 0; indexAdjs < adjs.length; indexAdjs++){
+      for(let indexNouns = 0; indexNouns < nouns.length; indexNouns++){
+console.log(pronouns[indexPronouns]+adjs[indexAdjs]+nouns[indexNouns]+".com")
+  }
+  }
+  }
--- src/app.js
+++ src/app.js
@@ -1,12 +1,6 @@
-  let pronouns = ['the', 'our'];
-  let adjs = ['great', 'big'];
-  let nouns = ['jogger', 'racoon'];
+import "bootstrap";
+import "./style.css";
 
-  for(let indexPronouns = 0; indexPronouns < pronouns.length; indexPronouns++){
-    for(let indexAdjs = 0; indexAdjs < adjs.length; indexAdjs++){
-      for(let indexNouns = 0; indexNouns < nouns.length; indexNouns++){
-console.log(pronouns[indexPronouns]+adjs[indexAdjs]+nouns[indexNouns]+".com")
-  }
-  }
-  }
 
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
